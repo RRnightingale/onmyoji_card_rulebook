59406c936652eab4b57ae4fbb4567a528f50e644
RRnightingale <103926118+RRnightingale@users.noreply.github.com>
2022-04-19 17:58:42 +0800

    setting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Omnyoji_card_game'
+copyright = '2022, RRnightingale'
+author = 'RRnightingale'
 
 release = '0.1'
 version = '0.1.0'
